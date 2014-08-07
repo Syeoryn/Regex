6b3efc5bae84d63a75aac81e855032e731cd5638
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-07 11:17:30 -0700

    Describes solveCrosswordPuzzle

---
+++ solveCrosswordPuzzle.js
@@ -0,0 +1,11 @@
+// solveCrosswordPuzzle is a function that takes a word with 
+// unknown letters as an input (e.g. jav_scr___),
+// and returns a list of all words in the english language that
+// fit the letters given
+
+// All english words are saved to the dictionary variable,
+// loaded in regex.html/ dictionary.js
+
+var solveCrosswordPuzzle = function(letters){
+  
+}
