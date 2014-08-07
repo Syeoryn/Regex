dbcd2d5770dc359d787531aa7e6d1c0a5e6b0165
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-07 11:09:30 -0700

    Describes findTLDRs

---
+++ findTLDRs.js
@@ -0,0 +1,11 @@
+// findTLDRs is a function that takes a string as an input,
+// and returns a list of the text following a tldr
+// but before a new line.
+
+// TLDR can come in many forms.  
+// Try to make your function as robust as possible.
+// For instance:  tldr, tl;dr, tl dr, TLDR, TL;DR...
+
+var findTLDRs = function(text){
+
+}
