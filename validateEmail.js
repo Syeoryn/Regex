3c8f0e00920b7386347ea527c6b3b9bc2c0f35b3
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-07 03:03:25 -0700

    Simplifies valid email description

--- validateEmail.js
+++ validateEmail.js
@@ -5,12 +5,11 @@
 //       local part (simplified):
 //                   Any english letter (A-Z, a-z)
 //                   Digits 0-9
-//                   Special characters: ! # $ % & ' * + - / = ? ^ _ ` { | } ~
+//                   Special characters: ! # $ % & ' ~
 //                   Period . as long as it isn't the first or last letter
 //                                and it doesn't appear two+ times consecutively
 //        domain part (simplified):
 //                   Any number of english letters, then ., finally more english letters
-//                   Or [<ip_address>]
 
 
 var validateEmail = function(emailAddress){
