6a0ff44d61c7e75cc5e972916ad1ca4340044bce
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-07 11:40:18 -0700

    Describes validatePhoneNumber

---
+++ validatePhoneNumber.js
@@ -0,0 +1,12 @@
+// validatePhoneNumber takes a string as an input and 
+// returns either true or the string if it is a valid phone number
+
+// Phone numbers may be in a few formats.  Examples:
+// (###) ###-####
+// ###.###.####
+// ###-###-####
+// Try to allow for each of these formats in your function
+
+var validatePhoneNumber = function(phoneNumber){
+
+}
