8440baddcc4a0afaec177a76b3eef1d31b0313ee
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-07 01:47:42 -0700

    Describes validateEmail function

---
+++ validateEmail.js
@@ -0,0 +1,18 @@
+// Write a function that takes a string as an input
+// and validates that the string is a valid email address
+
+// Email may contain:
+//       local part (simplified):
+//                   Any english letter (A-Z, a-z)
+//                   Digits 0-9
+//                   Special characters: ! # $ % & ' * + - / = ? ^ _ ` { | } ~
+//                   Period . as long as it isn't the first or last letter
+//                                and it doesn't appear two+ times consecutively
+//        domain part (simplified):
+//                   Any number of english letters, then ., finally more english letters
+//                   Or [<ip_address>]
+
+
+var validateEmail = function(emailAddress){
+
+}
