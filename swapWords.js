2b35bc3376d9c17354bc70904a2aaae3a41380c6
Drew Cuthbertson <syeoryn@gmail.com>
2014-08-07 11:37:34 -0700

    Describes swapWords

---
+++ swapWords.js
@@ -0,0 +1,9 @@
+// swapWords takes a string containing at least two words,
+// swaps the first two words, then returns the new string.
+
+// Could be useful for reformatting a list of names from
+// firstname lastname to lastname, firstname
+
+var swapWords = function(string){
+
+}
